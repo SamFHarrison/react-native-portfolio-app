ab945bbf2feb06bffc6126119a6b2037dfb66c21
Sam F-Harrison <sgfletcherharrison@gmail.com>
2023-12-06 18:25:33 +0000

    WIP: Home page

--- app/index.js
+++ app/index.js
@@ -1,11 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, SafeAreaView, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
+  const route = useRouter();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.flex}>
+      <Stack.screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.lightWhite,
+            headerShadowVisible: false,
+          },
+        }}
+      />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+  bgLightWhite: {
+    backgroundColor: COLORS.lightWhite,
+  },
+});
 
 export default Home;
